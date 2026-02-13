b1b68b0e8c5f461ad2637f7c2e59e20fc787beec
Michael Amiethyst <206824759@tfayd.com>
2026-02-13 12:53:13 -0500

    0.21.0 - Created argument parsing Bash tests

--- src/test/kotlin/org/bashpile/core/maintests/SwitchMainTest.kt
+++ src/test/kotlin/org/bashpile/core/maintests/SwitchMainTest.kt
@@ -33,11 +33,70 @@ class SwitchMainTest : MainTest() {
     }
 
     @Test
-    fun bashArguments_worksAsExpected() {
+    fun bashArguments_withGetopt_worksAsExpected() {
         val bashScript = """
-            ...
+            VALID_ARGS=$(getopt -o abg:d: --long alpha,beta,gamma:,delta: -- "$@")
+            if [[ $? -ne 0 ]]; then
+                exit 1;
+            fi
+
+            eval set -- "${'$'}VALID_ARGS"
+            while [ : ]; do
+              case "$1" in
+                -a | --alpha)
+                    printf "Processing 'alpha' option"
+                    shift
+                    ;;
+                -b | --beta)
+                    printf "Processing 'beta' option"
+                    shift
+                    ;;
+                -g | --gamma)
+                    printf "Processing 'gamma' option. Input argument is '$2'"
+                    shift 2
+                    ;;
+                -d | --delta)
+                    printf "Processing 'delta' option. Input argument is '$2'"
+                    shift 2
+                    ;;
+                --) shift; 
+                    break 
+                    ;;
+              esac
+            done
 
         """.trimIndent()
-        bashScript.runCommand(arguments = listOf("start")).assertRenderProduces("Starting service...\n")
+        bashScript.runCommand(arguments = listOf("--alpha")).assertRenderProduces("Processing 'alpha' option\n")
+    }
+
+    /** When we implement while statements we'll handle argument parsing this way */
+    @Test
+    fun bashArguments_withWhile_worksAsExpected() {
+        val bashScript = """
+            while [[ $# -gt 0 ]]; do
+              key="$1"
+            
+              case ${'$'}key in
+                -e|--env)
+                  ENV_VAR="$2"
+                  printf "Environment variable set to: %s" "${'$'}ENV_VAR"
+                  shift # shift once for the key
+                  shift # shift a second time for the value
+                  ;;
+                -d|--delete)
+                  DELETE=true
+                  printf "Delete flag set to: ${'$'}DELETE"
+                  shift # shift once for the flag
+                  ;;
+                *)    # Unknown option or positional argument
+                  # Handle positional arguments or error out
+                  printf "Unknown option %s" "$1"
+                  exit 1
+                  ;;
+              esac
+            done
+
+        """.trimIndent()
+        bashScript.runCommand(arguments = listOf("--delete")).assertRenderProduces("Delete flag set to: true\n")
     }
 }
