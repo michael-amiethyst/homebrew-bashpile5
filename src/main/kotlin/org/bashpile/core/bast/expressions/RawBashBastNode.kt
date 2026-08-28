dbf469f0f5513202d3315a5c0391632983309206
michael-amiethyst <michaelamiethyst@gmail.com>
2026-08-28 12:22:17 -0700

    0.22.0 - Moved RawBastNode out of Substrings package

--- src/main/kotlin/org/bashpile/core/bast/expressions/RawBashBastNode.kt
+++ src/main/kotlin/org/bashpile/core/bast/expressions/RawBashBastNode.kt
@@ -1,15 +1,15 @@
-package org.bashpile.core.bast.expressions.shellstrings
+package org.bashpile.core.bast.expressions
 
 import org.bashpile.core.bast.BastNode
 import org.bashpile.core.engine.RenderOptions
 
 /**
- * This is for literal Bash -- not a Subshell or using Command Substitution.
+ * This is for literal Bash -- not necessarily a Subshell or using Command Substitution.
+ * The literal Bash MAY contain a Subshell or Command Substitution though.
  *
  * `b(cd)` becomes `cd`
  */
-// TODO subclass BastNode directly
-class RawBashBastNode(children: List<BastNode> = listOf()) : ShellStringBastNode(children) {
+class RawBashBastNode(children: List<BastNode> = listOf()) : BastNode(children.toMutableList()) {
     override fun render(options: RenderOptions): String {
         return children.joinToString("") { it.render(options) }
     }
